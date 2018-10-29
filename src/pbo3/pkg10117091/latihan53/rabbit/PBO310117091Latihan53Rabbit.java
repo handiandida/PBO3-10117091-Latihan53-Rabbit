d553a8d18f14af807558a17e27ce7a5c7ee21650
Dida Handian <handiandida@gmail.com>
2018-10-29 20:49:27 +0700

    Rabbit

---
+++ src/pbo3/pkg10117091/latihan53/rabbit/PBO310117091Latihan53Rabbit.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan53.rabbit;
+
+/**
+ *
+ * @author 
+ * NAMA  : Dida Handian
+ * Kelas : PBO3
+ * NIM   : 10117091
+ * Deskripsi Tugas : Membuat Program Klasifikasi Binatang
+ *  
+ */
+public class PBO310117091Latihan53Rabbit {
+
+    /**
+     */
+    protected static boolean vegetarian;
+    protected static String eats;
+    protected static int noOfLegs;
+    private static String color;
+    private static String name;
+    
+    
+    public static void main(String[] args) {
+       Animal animal = new Animal(false, "grass", 4);
+       Rabbit rabbit = new Rabbit("grey", "Peter", false, "grey", 4);
+       System.out.println("Hello, His name is " + rabbit.getName());
+       System.out.println(rabbit.getName() + " is Vegetarian? " + 
+                          rabbit.vegetarian);
+       System.out.println(rabbit.getName() + " eats " + animal.eats);
+       System.out.println(rabbit.getName() + " has " + rabbit.noOfLegs + 
+                          " legs.");
+       System.out.println(rabbit.getName() + " color is " + rabbit.getColor());   
+        
+    }
+    
+}
